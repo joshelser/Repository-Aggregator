7864bb1aa416668f19f78c362733e3beae80306f
je2451 <josh.elser@gmail.com>
2009-05-22 00:16:58 -0400

    Added GPLv3 license information to the files

--- class/Repository.class.php
+++ class/Repository.class.php
@@ -1,5 +1,25 @@
 <?php
 
+/*
+    Copyright 2009, All Rights Reserved.
+
+    This file is part of Repository-Aggregator.
+
+    Repository-Aggregator is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Repository-Aggregator is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Repository-Aggregator.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 require_once( 'lib/functions.lib.php' );
 
 class Repository {
--- config/setup.sql
+++ config/setup.sql
@@ -1,3 +1,22 @@
+--     Copyright 2009, All Rights Reserved.
+
+--     This file is part of Repository-Aggregator.
+
+--     Repository-Aggregator is free software: you can redistribute it and/or modify
+--     it under the terms of the GNU General Public License as published by
+--     the Free Software Foundation, either version 3 of the License, or
+--     (at your option) any later version.
+
+--     Repository-Aggregator is distributed in the hope that it will be useful,
+--     but WITHOUT ANY WARRANTY; without even the implied warranty of
+--     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--     GNU General Public License for more details.
+
+--     You should have received a copy of the GNU General Public License
+--     along with Repository-Aggregator.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 CREATE TABLE repositories (
        repoId int(5) NOT NULL auto_increment,
        url varchar(250) NOT NULL,
