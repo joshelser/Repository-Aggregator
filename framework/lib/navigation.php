991fb1b9532385370faea23a231caf2ce476fd76
je2451 <josh.elser@gmail.com>
2009-05-27 12:23:00 -0400

    The start of a horizontal CSS drop-down menu

--- framework/lib/navigation.php
+++ framework/lib/navigation.php
@@ -8,13 +8,12 @@ $login = '<li><a href="login.php">Login</a></li>';
 
 if( isset( $_SESSION['username'] ) ){
   $login = <<<EOT
-<li><span>Welcome, {$_SESSION['username']}</span></li>
-<li id="repositoryMenu" onclick="showMenu( 'repository' )">Repositories</li>
-<div id="repositorySubMenu" class="headerSubMenu">
-<a href="viewRepositories.php">View Repositories</a>
-<br />
-<a href="addRepository.php">Add a Repository</a>
-</div>
+<li id="repositoryMenu">Repositories
+  <ul id="repositorySubMenu" class="headerSubMenu">
+    <li><a href="viewRepositories.php">View Repositories</a></li>
+    <li><a href="addRepository.php">Add a Repository</a></li>
+  </ul>
+</li>
 <li><a href="logout.php">Logout</a></li>
 EOT;
 }
