593df602b17a32c9e1ce86da3942f4fcd811403d
je2451 <josh.elser@gmail.com>
2009-06-03 21:58:05 -0400

    Visualize the commits for repository through viewCommits.php

---
+++ viewCommits.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
+    Copyright 2009, All Rights Reserved.
+
+    This file is part of Repository-Aggregator.
+
+    Repository-Aggregator is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Repository-Aggregator is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Repository-Aggregator.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+require_once( 'lib/functions.lib.php' );
+
+$framework = frameworkDir();
+
+require_once( 'config/Config.class.php' );
+require_once( $framework.'/lib/authentication.php' );
+
+authorize();			/* Are you allowed here? */
+
+// Library File
+require_once( $framework.'/lib/library.php' );
+
+
+$title = 'View Commits';
+$subtitle = Config::get( 'siteName' );
+$content = '';
+
+$commits = getCommits( $_GET['id'] ); /* Get commits */
+
+for( $i = 0; $i < count( $commits ); $i++ ) {
+  $content .= <<<EOT
+<div id="{$i}">
+  <h3>{$commits[$i][commitMessage]}</h3>
+  <p>Commit: {$commits[$i][commitVal]}</p>
+  <p>Date: {$commits[$i][commitDateTime]}</p>
+	<div id="files{$i}">
+		<p>Files changed</p>
+EOT;
+
+	for( $j = 0; $j < count( $commits[$i][fileChanges] ); $j++ ){
+		$content .= <<<EOT
+		  <div class="file">
+				<p>Filename: {$commits[$i][fileChanges][$j][file]}</p>
+				<p>Insertions: {$commits[$i][fileChanges][$j][insertions]}</p>
+				<p>Deletions: {$commits[$i][fileChanges][$j][deletions]}</p>
+			</div>
+EOT;
+		}
+	
+	$content .= <<<EOT
+	</div>
+</div>
+EOT;
+
+  if( $i != count( $commits ) -1 ){
+    $content .= "\n<br/>\n";
+  }
+}
+  
+
+/*
+**   P U T    V A R S
+**    O N    P A G E
+*/
+	head($title,$style,$scripts);
+	body($header,$subtitle,$content,$navigation);
+	foot($footer,$dbc);
+
+
+?>
