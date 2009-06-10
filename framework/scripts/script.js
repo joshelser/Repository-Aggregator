99d02451b0a0df77e43405bedd6409975089ab53
je2451 <josh.elser@gmail.com>
2009-06-09 20:13:27 -0400

    Added some images to the commmits page.

--- framework/scripts/script.js
+++ framework/scripts/script.js
@@ -1,10 +1,13 @@
 function showFileChanges( id ) {
 	var name = '#filechange' + id;
+	var image = '#image' + id;
 	if( $(name).css('display') == 'none' ) {
 		$(name).css( {'display': ''} );
+		$(image).attr( 'src', 'images/minussign.png' );
 	}
 	else {
 		$(name).css( {'display': 'none'} );
+		$(image).attr( 'src', 'images/plussign.png' );
 	}
 }
 
