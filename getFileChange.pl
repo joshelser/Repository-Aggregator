fdce366e76d20c4609d023fc6930fc05a8ea708d
je2451 <josh.elser@gmail.com>
2009-06-13 21:54:48 -0400

    First draft of the viewFileChange page

--- getFileChange.pl
+++ getFileChange.pl
@@ -21,22 +21,19 @@
 use strict;
 use warnings;
 
+# DBI and Ini parsing
 use DBI;
-use Git::Wrapper;
 use Config::Abstract::Ini;
 
 my $ini = new Config::Abstract::Ini( 'config/config.ini.php' );
 my %values = $ini->get_entry( '' );
 
+# Make sure the script is used properly
 if( @ARGV != 3 ) {
 	die( "Usage: ./getFileChange.pl repositoryId revisionId filename\n" );
 }
 
 my ( $repoId, $revId, $filename ) = @ARGV;
-
-if( $revId !~ m/(\d|[a-z]){40}/ ){
-	die 'Invalid commit ID was given';
-}
 
 # Assign values
 my $data_source = 'dbi:mysql:'. $values{'dbName'} .':'. $values{'dbHostname'};
@@ -50,6 +47,7 @@ my $dbh = DBI->connect($data_source, $user, $pass, {
 		       }) 
     or die "Can't connect to the database: $DBI::errstr\n";
 
+# Get the local directory for the repository
 my $sql = 'SELECT localDir FROM repositories WHERE repoId=' . $dbh->quote( $repoId ) . ' LIMIT 1';
 
 my $sth = $dbh->prepare( $sql );
@@ -57,7 +55,28 @@ $sth->execute();
 
 my @row = $sth->fetchrow_array();
 
-my $repoDir = $values{ 'repositoryDirectory' } . '/' . $row[0];
-my $output = `cd $repoDir && git show $revId $filename`;
+$sql = 'SELECT commitVal FROM commits WHERE commitId = ' . $dbh->quote( $revId ) . ' LIMIT 1';
 
-print $output."\n";
+$sth = $dbh->prepare( $sql );
+$sth->execute();
+
+my @row2 = $sth->fetchrow_array();
+
+# Sanity check on the git ID given
+if( $row2[0] !~ m/(\d|[a-z]){40}/ ){
+	die 'Invalid commit ID was given';
+}
+
+# For now, just execute the git command as a shell script
+my $repoDir = $values{ 'repositoryDirectory' } . '/' . $row[0];
+my $output = `cd $repoDir && git show --pretty=format:"%n" $row2[0] $filename`;
+
+print ltrim( $output );
+
+# Left trim function to remove leading whitespace
+sub ltrim {
+	my $string = shift;
+	$string =~ s/^\s+//;
+	return $string;
+}
+
---
+++ viewFileChange.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+    Copyright 2009, All Rights Reserved.
+
+    This file is part of Repository-Aggregator.
+
+    Repository-Aggregator is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Repository-Aggregator is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Repository-Aggregator.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+if( !isset( $_GET['commitId'] ) ) {
+	die( 'Must specify a commit to view' );
+}
+
+if( !isset( $_GET['file'] ) ) {
+	die( 'Must specify a filename' );
+}
+
+require_once( 'config/Config.class.php' );
+
+// Library File
+include 'framework/lib/library.php';
+
+
+$title = 'View File Change';
+$subtitle = Config::get( 'siteName' );
+
+$commitId = $_GET['commitId'];
+$file = $_GET['file'];
+$repoId = $_GET['repoId'];
+$diff = shell_exec( "./getFileChange.pl $repoId '$commitId' '$file'" );
+
+$diff = nl2br( htmlspecialchars( $diff ) );
+
+$content = <<<EOT
+<div class="indent">
+	<h3 class="indent">{$file}</h3>
+	<div id="code">
+		<code>
+			{$diff}
+		</code>
+	</div>
+</div>
+EOT;
+
+/*
+**   P U T    V A R S
+**    O N    P A G E
+*/
+	head($title,$style,$scripts);
+	body($header,$subtitle,$content,$navigation);
+	foot($footer,$dbc);
+?>
